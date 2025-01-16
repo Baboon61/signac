1eb526c21ba2638f98839abfb3d611fd62874008
timoast <4591688+timoast@users.noreply.github.com>
2025-01-16 12:31:17 +0800

    Update tests

--- tests/testthat/test-motifs.R
+++ tests/testthat/test-motifs.R
@@ -2,16 +2,17 @@ pwm <- readRDS("../testdata/pwm_2motifs.rds")
 genome.fasta <- system.file("extdata", "chr1_start.fa", package = "Signac")
 genome <- Rsamtools::FaFile(genome.fasta)
 
-# Cannot be tested if motifmatchr is not a dependency
-# test_that("AddMotifs works", {
-#   motif <- AddMotifs(
-#     atac_small[["peaks"]],
-#     genome,
-#     pwm,
-#     verbose = FALSE
-#   )
-#   expect_equal(dim(Motifs(motif)), c(323, 2))
-# })
+
+test_that("AddMotifs works", {
+  skip_if_not_installed("motifmatchr")
+  motif <- AddMotifs(
+    atac_small[["peaks"]],
+    genome,
+    pwm,
+    verbose = FALSE
+  )
+  expect_equal(dim(Motifs(motif)), c(323, 2))
+})
 
 test_that("AddMotifs works with fakechr", {
   fpath <- system.file("extdata", "fragments.tsv.gz", package = "Signac")
@@ -29,35 +30,35 @@ test_that("AddMotifs works with fakechr", {
     keep_all_features = TRUE,
     verbose = FALSE
   )
-  object <- CreateChromatinAssay(
+  expect_warning(object <- CreateChromatinAssay(
     counts = mat,
     sep = c(":", "-"),
     fragments = fragments,
     verbose = FALSE
-  )
-  # This requires motifmatchr
-  # motif <- AddMotifs.default(
-  #   granges(object)[100:nrow(object)],
-  #   genome,
-  #   pwm,
-  #   verbose = FALSE
-  # )
+  ))
+  skip_if_not_installed("motifmatchr")
+  motif <- suppressWarnings(AddMotifs(
+    granges(object)[100:nrow(object)],
+    genome,
+    pwm,
+    verbose = FALSE
+  ))
   motif <- readRDS("../testdata/motifObj_2_225.rds")
   expect_equal(dim(motif), c(225, 2))
-  test <- SetAssayData(
+  expect_warning(test <- SetAssayData(
     object = object,
     layer = "motifs",
     new.data = motif,
     verbose = FALSE
-  )
+  ))
   expect_equal(dim(Motifs(test)), c(324, 2))
   # If some features are not in the ChromatinAssay
   # They should be removed
-  test <- SetAssayData(
+  expect_warning(test <- SetAssayData(
     object = atac_small,
     layer = "motifs",
     new.data = motif,
     verbose = FALSE
-  )
+  ))
   expect_equal(dim(Motifs(test)), c(323, 2))
 })
